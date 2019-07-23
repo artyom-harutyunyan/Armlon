fcf6bf26f0e8d6b56c28b219e103243b7f68bf54
unknown <h.artyom26@gmail.com>
2019-07-23 14:05:30 +0400

    application framework

---
+++ armlon-app/src/Pages/Company/Company.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.sass';
+
+export default class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
